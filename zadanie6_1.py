332b61132a8e74c9b4ebefcdb5436d5a89bd1e6f
msiembab <msiembab@users.noreply.github.com>
2021-05-09 13:56:40 +0200

    Nowy commit zadania6_1

--- zadanie6_1.py
+++ zadanie6_1.py
@@ -4,6 +4,6 @@ wsparcie_no="nie wspierana"
 wsparcie_yes="aktualne wsparcie"
 
 if wersja == 8.0:
-    print ("ta wersja {0} jest {1}".format(nazwa_os,wsparcie_no))
+    print ("Ta wersja {0} jest {1}".format(nazwa_os,wsparcie_no))
 if wersja > 8.0:
     print ("Ta wersja {0} ma {1}".format(nazwa_os,wsparcie_yes))
