b47840d9f8ff0fed8315e286b892fb3edb37b782
msiembab <msiembab@users.noreply.github.com>
2021-05-09 13:51:54 +0200

    Commit zadania6_1

--- zadanie6_1.py
+++ zadanie6_1.py
@@ -1,9 +1,9 @@
 nazwa_os='Windows'
-wersja=8
+wersja=8.1
 wsparcie_no="nie wspierana"
 wsparcie_yes="aktualne wsparcie"
 
-if wersja <= 8
-    print ("Wersja {0} jest {1}".format(nazwa_os,wersja_no))
-if wersja > 8
-  print ("Wersja {0} ma {1}".format(nazwa_os,wersja_yes))
+if wersja == 8.0:
+    print ("ta wersja {0} jest {1}".format(nazwa_os,wsparcie_no))
+if wersja > 8.0:
+    print ("Ta wersja {0} ma {1}".format(nazwa_os,wsparcie_yes))
